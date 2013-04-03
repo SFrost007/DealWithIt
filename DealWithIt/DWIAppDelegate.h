306616ddfc7c65d3094879ab2d43c97ea7a1fa88
simon.frost <simon.frost@accenture.com>
2013-04-03 15:50:04 +0100

    Initial Commit

---
+++ DealWithIt/DWIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DWIAppDelegate.h
+//  DealWithIt
+//
+//  Created by Simon Frost on 03/04/2013.
+//  Copyright (c) 2013 Orangeninja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DWIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
